5cb06053f68fc0efc1a4cf6332ebbaa548c50f42
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-02-17 23:49:25 -0500

    deleted 3 useless css file

--- front-end/src/components/NavbarItem/index.jsx
+++ front-end/src/components/NavbarItem/index.jsx
@@ -6,11 +6,11 @@ const NavbarItem = () => {
     const items = ["Shop", "New Arrivals", "Sales", "Journal"]; // Corrected spelling of "Journal"
   
     return (
-      <div className="flex sm:flex-col flex-row sm:gap-5 items-center justify-start w-auto sm:w-full">
+      <div className="flex sm:flex-col flex-row sm:gap-5 items-left justify-start w-auto sm:w-full">
         {items.map((item) => (
-          <div key={item} className="flex flex-col items-center justify-center sm:px-5 px-[21px] w-auto">
-            <div className="flex flex-col items-center justify-center py-1.5 w-auto">
-              <Text className="text-[13px] text-black w-auto" size="txtStyreneAWebRegular13">
+          <div key={item} className="flex flex-col justify-start sm:px-5 px-[21px] w-auto">
+            <div className="flex flex-col justify-start py-1.5 w-auto">
+              <Text className="text-[20px] text-black w-auto" size="txtStyreneAWebRegular13">
                 {item}
               </Text>
             </div>
--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import "./styles/color.css";
-import "./styles/font.css";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./styles/index.css";
 import "./styles/tailwind.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
